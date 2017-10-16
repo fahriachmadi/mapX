485bc115e4c1f7999103c74fc08fe300c1a6bf68
Fahri Achmadi <fahri.conqueror@gmail.com>
2017-10-17 01:49:36 +0700

    add project MapX

---
+++ app/src/main/java/com/tekmob/mapx/MainActivity.java
@@ -0,0 +1,15 @@
+package com.tekmob.mapx;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Conqueror on 10/16/2017.
+ */
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
